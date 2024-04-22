f80d26b52bd022307b429c77e040058edcb874bd
stefanpost268 <97079064@st.deltion.nl>
2024-04-22 13:51:11 +0200

    NER-103 ADD: read h2010 sensor

---
+++ arduino/arduino.ino
@@ -0,0 +1,13 @@
+const int sensorPin = A0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensorValue = analogRead(sensorPin);
+  
+  Serial.print("Sensor value: ");
+  Serial.println(sensorValue);
+  delay(1000);
+}
