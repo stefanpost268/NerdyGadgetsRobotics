eeeee5dc15a74cfbc31d8be1d79ed02eb84db466
HiddeXA <hiddealberts@gmail.com>
2024-04-23 14:38:03 +0200

    NER-99 ADD: added functions to add products to the visual grid and dummy data

--- NerdyGadgetsRobotics/src/visualComponents/WarehouseMap.java
+++ NerdyGadgetsRobotics/src/visualComponents/WarehouseMap.java
@@ -1,7 +1,10 @@
 package visualComponents;
 
+import objects.GridProduct;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class WarehouseMap extends JPanel {
 
@@ -9,15 +12,21 @@ public class WarehouseMap extends JPanel {
     private int gridWidth;
     private int width = 670;
     private int height = 415;
+    private ArrayList<GridProduct> gridProducts = new ArrayList<GridProduct>();
 
     public WarehouseMap(int gridHeight, int gridWidth) {
         this.gridHeight = gridHeight;
         this.gridWidth = gridWidth;
+
+        //dummy data
+        this.addGridProduct(1,1,Color.BLUE);
+        this.addGridProduct(3,2,Color.RED);
+        this.addGridProduct(3,5,Color.YELLOW);
+        this.addGridProduct(5,5,Color.GREEN);
     }
 
     public WarehouseMap(int gridHeight, int gridWidth, int width, int height) {
-        this.gridHeight = gridHeight;
-        this.gridWidth = gridWidth;
+        this(gridHeight, gridWidth);
         this.width = width;
         this.height = height;
     }
@@ -50,9 +59,44 @@ public class WarehouseMap extends JPanel {
         //Numbers
         for (int i = 1; i < height; i++) g.drawString(String.valueOf(i), blockWidth/2 - 20, blockHeight * i );
 
+        for (GridProduct gridProduct : gridProducts) gridProduct.draw(g, this);
+
+
     }
 
     private String getCharForNumber(int i) {
         return i > 0 && i < 27 ? String.valueOf((char)(i + 64)) : null;
+    }
+
+    public void addGridProduct(int x, int y, Color color) {
+        if (x > gridWidth || y > gridHeight) {
+            System.out.println("x: " + x + "y: " + y + " does not exist");
+            return;
+        }
+        gridProducts.add(new GridProduct(x,y,color));
+    }
+
+    public void clearGridProducts() {
+        gridProducts.clear();
+    }
+
+    public int getGridHeight() {
+        return gridHeight;
+    }
+
+    public int getGridWidth() {
+        return gridWidth;
+    }
+
+    public int getMapWidth() {
+        return width;
+    }
+
+    public int getMapHeight() {
+        return height;
+    }
+
+    public ArrayList<GridProduct> getGridProducts() {
+        return gridProducts;
     }
 }
