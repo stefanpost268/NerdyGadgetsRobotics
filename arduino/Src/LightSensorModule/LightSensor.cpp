e2adcb58bc1be48e266e1e77de2f49adfdc5ebc9
stefanpost268 <97079064@st.deltion.nl>
2024-04-23 13:36:08 +0200

    NER-103 ADD: emitWarehouseTiltedStatus on the LightSensorModule

--- arduino/Src/LightSensorModule/LightSensor.cpp
+++ arduino/Src/LightSensorModule/LightSensor.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include "LightSensor.h"
+#include "../../Modules/ArduinoJson-v7.0.4.h"
 
 LightSensor::LightSensor(int pin) {
     this->pin = pin;
@@ -8,4 +9,15 @@ LightSensor::LightSensor(int pin) {
 
 bool LightSensor::isActive() {
     return analogRead(pin) > 100;
+}
+
+void LightSensor::emitWarehouseTiltedStatus() {
+    DynamicJsonDocument doc(128);
+    doc["label"] = "state";
+    JsonObject data = doc.createNestedObject("data");
+    data["state"] = "SAFETY_MODE";
+    data["reason"] = "The warehouse has tilted";
+    String jsonString;
+    serializeJson(doc, jsonString);
+    Serial.println(jsonString);
 }
--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,37 +1,19 @@
 #include "Src/LightSensorModule/LightSensor.h"
-#include "Modules/ArduinoJson-v7.0.4.h"
 
 LightSensor lightSensor = LightSensor(A0);
-bool messageSent = false; // Flag to track if the message has been sent
-
-const int sensorPin = A0;
+bool safetyMode = false;
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  if (!lightSensor.isActive() && !messageSent) { // Check if the sensor is inactive and the message hasn't been sent
-    // Create a DynamicJsonDocument
-    DynamicJsonDocument doc(128); // Adjust the size as per your data
-
-    // Add data to the JSON document
-    doc["label"] = "state";
-
-    // Create an object for the data
-    JsonObject data = doc.createNestedObject("data");
-    data["state"] = "SAFETY_MODE";
-    data["reason"] = "Light sensor is active";
-
-    // Serialize the JSON document to a string
-    String jsonString;
-    serializeJson(doc, jsonString);
-
-    // Print the JSON string to the serial monitor
-    Serial.println(jsonString);
-
-    messageSent = true; // Set the flag to indicate that the message has been sent
+  if(safetyMode) {
+    return;
   }
 
-  delay(1000);
+  if (!lightSensor.isActive()) {
+    lightSensor.emitWarehouseTiltedStatus();
+    safetyMode = true;
+  }
 }
