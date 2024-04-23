f5678d7b3a216f208d83920e9e5debe6a56aae1f
stefanpost268 <97079064@st.deltion.nl>
2024-04-23 11:34:15 +0200

    NER-103 ADD: jSerialComm dependencie with gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.fazecast:jSerialComm:[2.0.0,3.0.0)")
 }
 
 tasks.test {
