a3ee69fc005709bfaf5641a8074ee425c9a3e3ed
kolsm <kol.smits@hotmail.com>
2024-04-24 11:59:26 +0200

    NER-97 IMP: remove LED implementation  & implements json

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,27 +1,15 @@
-#include "Src/LightSensorModule/LightSensor.h"
+#include "../arduino/Src/Modules/LightSensorModule/LightSensor.h"
 #include "Vendor/ArduinoJson-v7.0.4.h"
+#include "Src/Modules/JsonRobotModule/JsonRobot.cpp"
+#include "../arduino/Src/Enum/RobotStateEnum.h"
+#include "../arduino/Src/Enum/LabelEnum.h"
+#include  "../arduino/Src/Modules/EmergencyButtonModule/EmergencyButton.cpp"
 
-// emergencyStop
-#define LED 4
+// EmergencyStop
 #define emergencyStop 8
 #define resetButton 10
 
 bool SAFETY_MODE = false;
-int buttonState = digitalRead(emergencyStop);
-void LEDstate() {
-  if (SAFETY_MODE) {
-    turnOnLED();
-  } else {
-    turnOffLED();
-  }
-}
-
-void turnOnLED() {
-  digitalWrite(LED, HIGH);
-}
-void turnOffLED() {
-  digitalWrite(LED, LOW);
-}
 
 // lightSensor
 LightSensor lightSensor = LightSensor(A0);
@@ -30,14 +18,14 @@ bool messageSent = false; // Flag to track if the message has been sent
 const int sensorPin = A0;
 
 void setup() {
-  pinMode(LED, OUTPUT);
   pinMode(resetButton, INPUT_PULLUP);
   pinMode(emergencyStop, INPUT_PULLUP);
   Serial.begin(9600);
 }
 
 void loop() {
-SAFETY();
+
+
   if (!lightSensor.isActive() && !messageSent) { // Check if the sensor is inactive and the message hasn't been sent
     // Create a DynamicJsonDocument
     DynamicJsonDocument doc(128); // Adjust the size as per your data
@@ -60,42 +48,17 @@ SAFETY();
     messageSent = true; // Set the flag to indicate that the message has been sent
   }
 
-//emergencyStop
-if (digitalRead(emergencyStop) == HIGH && !messageSent) { // Check if the sensor is inactive and the message hasn't been sent
-    // Create a DynamicJsonDocument
-    DynamicJsonDocument doc(128); // Adjust the size as per your data
-
-    // Add data to the JSON document
-    doc["label"] = "state";
-
-    // Create an object for the data
-    JsonObject data = doc.createNestedObject("data");
-    data["state"] = "SAFETY_MODE";
-    data["reason"] = "Emergency stop is pressed!";
-
-    // Serialize the JSON document to a string
-    String jsonString;
-    serializeJson(doc, jsonString);
-
-    // Print the JSON string to the serial monitor
-    Serial.println(jsonString);
-
-    messageSent = true; // Set the flag to indicate that the message has been sent
+  //emergencyStop
+  if (digitalRead(emergencyStop) == HIGH && !SAFETY_MODE && !digitalRead(resetButton)) { 
+    EmitRobotState(STATE, EMERGENCY_STOP);
   }
 
-  
+  // MANUAL
+  if (digitalRead(resetButton) == HIGH && SAFETY_MODE && !digitalRead(emergencyStop)) { 
+   EmitRobotState(STATE, MANUAL_MODE);
+  }
+
 }
 
-void SAFETY() {
-  LEDstate();
-  if (digitalRead(emergencyStop) == HIGH) {
-    SAFETY_MODE = true;
-  }
-  if (digitalRead(resetButton) == HIGH && digitalRead(emergencyStop) == LOW) {
-    SAFETY_MODE = false;
-  }
-  if (SAFETY_MODE == true) {
-    //stop robot
-  }
-}
+
 
