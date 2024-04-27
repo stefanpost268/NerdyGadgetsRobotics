edb7c8667ba5a3f0f6d4de5eca52131a7ccf7a17
stefanpost268 <97079064@st.deltion.nl>
2024-04-27 21:24:53 +0200

    NER-117 ADD: Flyway migration dependencie

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("com.fazecast:jSerialComm:[2.0.0,3.0.0)")
+    implementation("org.flywaydb:flyway-core:10.11.1")
 }
 
 tasks.test {
