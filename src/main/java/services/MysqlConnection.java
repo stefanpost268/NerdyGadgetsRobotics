c610fcc33b043b45969ac0d99416ba1cf7305769
stefanpost268 <97079064@st.deltion.nl>
2024-04-28 16:36:48 +0200

    NER-91 ADD: getConnection method on MysqlConnection Service

--- src/main/java/services/MysqlConnection.java
+++ src/main/java/services/MysqlConnection.java
@@ -1,16 +1,17 @@
 package services;
 
-import java.sql.*;
-import java.util.List;
-import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class MysqlConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/nerdygadgets";
     private static final String USER = "root";
     private static final String PASSWORD = "";
-
     private Connection connection;
 
+    /**
+     * Create a new MySQL connection
+     */
     public MysqlConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -20,24 +21,11 @@ public class MysqlConnection {
         }
     }
 
-    public List<Object[]> executeQuery(String query) {
-        List<Object[]> result = new ArrayList<>();
-        try {
-            Statement statement = this.connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(query);
-            ResultSetMetaData metaData = resultSet.getMetaData();
-            int columnCount = metaData.getColumnCount();
-            while (resultSet.next()) {
-                Object[] row = new Object[columnCount];
-                for (int i = 1; i <= columnCount; i++) {
-                    row[i - 1] = resultSet.getObject(i);
-                }
-                result.add(row);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return result;
+    /**
+     * Get the current mysql connection
+     * @return Connection
+     */
+    public Connection getConnection() {
+        return this.connection;
     }
 }
