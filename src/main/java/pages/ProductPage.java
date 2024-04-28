951832b3ffa472f5a86c591366d4706be37bc22d
stefanpost268 <97079064@st.deltion.nl>
2024-04-28 19:31:10 +0200

    NER-91 ADD: searchPanel & refresh table data when searchButton clicked

--- src/main/java/pages/ProductPage.java
+++ src/main/java/pages/ProductPage.java
@@ -1,27 +1,63 @@
 package pages;
 
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import java.awt.BorderLayout;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import models.StockItem;
 
-public class ProductPage extends JPanel {
+public class ProductPage extends JPanel implements ActionListener {
+
+    private JTextField searchField = new JTextField(20);
+    private JButton searchButton = new JButton("Search");
+    private JTable table;
+    private StockItem stockItem = new StockItem();
+
     public ProductPage() {
-        StockItem stockItem = new StockItem();
+        setLayout(new BorderLayout());
 
-        JTable table = new JTable(
-            stockItem.toTableData(stockItem.get()),
-            stockItem.fillable()
-        );
+        JPanel searchPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        searchPanel.add(this.searchField);
+        searchPanel.add(this.searchButton);
+        add(searchPanel, BorderLayout.PAGE_START);
+        this.searchButton.addActionListener(this);
 
-        table.setFillsViewportHeight(true);
-        table.setDefaultEditor(Object.class, null);
+        renderTable();
+    }
 
-        JScrollPane scrollPane = new JScrollPane(table);
+    private void renderTable() {
+        search();
+
+        JScrollPane scrollPane = new JScrollPane(this.table);
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
-        setLayout(new BorderLayout());
+        this.table.setFillsViewportHeight(true);
+        this.table.setDefaultEditor(Object.class, null);
+
         add(scrollPane, BorderLayout.CENTER);
+    }
+
+    private void search() {
+        DefaultTableModel model = new DefaultTableModel(
+                this.stockItem.toTableData(stockItem.get()),
+                this.stockItem.fillable()
+        );
+
+        if(this.table == null) {
+            this.table = new JTable(model);
+        } else {
+            this.table.setModel(model);
+        }
+
+        this.table.repaint();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource().equals(this.searchButton)) {
+            search();
+        }
     }
 }
