2fade772170b9e3d0016dee493f016f7d118195b
stefanpost268 <97079064@st.deltion.nl>
2024-04-28 14:58:35 +0200

    NER-91 ADD: show data table of stockitems on ProductPage

--- src/main/java/pages/ProductPage.java
+++ src/main/java/pages/ProductPage.java
@@ -4,21 +4,35 @@ import services.MysqlConnection;
 
 import javax.swing.*;
 import java.util.List;
+import java.awt.*;
 
 public class ProductPage extends JPanel {
 
     private MysqlConnection mysql;
+    private List<Object[]> data;
 
     public ProductPage(MysqlConnection mysql) {
         this.mysql = mysql;
-        JLabel label = new JLabel("Vooraad");
-        List<Object[]> data = this.mysql.executeQuery("SELECT * FROM stockitemholdings;");
-        for (Object[] row : data) {
-            for (Object column : row) {
-                System.out.print(column + " ");
-            }
-            System.out.println();
+        this.data = this.mysql.executeQuery(
+                "SELECT " +
+                "StockItemID, StockItemName, UnitPrice, RecommendedRetailPrice, TypicalWeightPerUnit, QuantityPerOuter, Size" +
+                " FROM stockitems;"
+        );
+
+        String[] columnNames = {"Id", "Naam", "Inkoop prijs", "Verkoop prijs", "Gewicht (KG)", "Vooraad", "Locatie"};
+        Object[][] tableData = new Object[this.data.size()][columnNames.length];
+        for (int i = 0; i < this.data.size(); i++) {
+            tableData[i] = this.data.get(i);
         }
-        add(label);
+
+        JTable table = new JTable(tableData, columnNames);
+        table.setFillsViewportHeight(true);
+        table.setDefaultEditor(Object.class, null);
+
+        JScrollPane scrollPane = new JScrollPane(table);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+        setLayout(new BorderLayout());
+        add(scrollPane, BorderLayout.CENTER);
     }
 }
