caf84d11e3705b70875e6344091c851e551e20b6
stefanpost268 <97079064@st.deltion.nl>
2024-04-28 16:52:47 +0200

    NER-91 ADD: StockItem Model

---
+++ src/main/java/models/StockItem.java
@@ -0,0 +1,41 @@
+package models;
+
+import java.util.List;
+
+public class StockItem extends BaseModel<StockItem> {
+    public Integer StockItemID;
+    public String StockItemName;
+    public Integer UnitPrice;
+    public Integer RecommendedRetailPrice;
+    public Integer TypicalWeightPerUnit;
+    public Integer QuantityPerOuter;
+    public String Size;
+
+    @Override
+    public String[] fillable() {
+        return new String[] {
+            "StockItemID",
+            "StockItemName",
+            "UnitPrice",
+            "RecommendedRetailPrice",
+            "TypicalWeightPerUnit",
+            "QuantityPerOuter",
+            "Size"
+        };
+    }
+
+    @Override
+    public String getTableName() {
+        return "stockitems";
+    }
+
+    @Override
+    public StockItem createInstance() {
+        return new StockItem();
+    }
+
+    public List<StockItem> get() {
+        return this.executeQuery("SELECT * FROM " + this.getTableName());
+    }
+
+}
