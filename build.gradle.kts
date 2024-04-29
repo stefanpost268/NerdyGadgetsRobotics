11e8da4958cfb01767464fbbec76334095b17f77
stefanpost268 <97079064@st.deltion.nl>
2024-04-29 20:56:12 +0200

    NER-106 ADD: flying-saucer-pdf dependencie

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("com.fazecast:jSerialComm:[2.0.0,3.0.0)")
     implementation("com.mysql:mysql-connector-j:8.3.0")
+    implementation("org.xhtmlrenderer:flying-saucer-pdf:9.7.2")
 }
 
 tasks.test {
