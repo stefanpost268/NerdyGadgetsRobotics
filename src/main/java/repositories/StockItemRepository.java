f1063022969c3128ea8d50ab2fb6e775cf580dd6
stefanpost268 <97079064@st.deltion.nl>
2024-05-06 13:01:24 +0200

    NER-123 ADD: findByStockItemNameContaining on StockItemRepository

--- src/main/java/repositories/StockItemRepository.java
+++ src/main/java/repositories/StockItemRepository.java
@@ -3,6 +3,7 @@ package repositories;
 import models.StockItem;
 import org.springframework.data.repository.CrudRepository;
 
-public interface StockItemRepository extends CrudRepository<StockItem, Integer> {
-    Iterable<StockItem> findByStockItemName(String stockItemName);
+public interface StockItemRepository extends CrudRepository<StockItem, Integer>
+{
+    Iterable<StockItem> findByStockItemNameContaining(String stockItemName);
 }
