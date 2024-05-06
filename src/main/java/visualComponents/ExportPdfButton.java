b2ac81fa29e10d3ce9386e3359ab4b9f036df480
stefanpost268 <97079064@st.deltion.nl>
2024-05-06 15:31:42 +0200

    NER-106 ADD: default name export pdf & confirm overwrite dialog

--- src/main/java/visualComponents/ExportPdfButton.java
+++ src/main/java/visualComponents/ExportPdfButton.java
@@ -1,11 +1,11 @@
 package visualComponents;
 
-import javax.swing.JPanel;
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
+import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 import services.PdfRenderer;
+
+import java.io.File;
 
 public class ExportPdfButton extends JPanel {
     private JButton exportPdfButton = new JButton("Export PDF");
@@ -21,6 +21,8 @@ public class ExportPdfButton extends JPanel {
     private void savePdf() {
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setDialogTitle("Save PDF");
+        fileChooser.setSelectedFile(new File("export.pdf"));
+
         fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
         fileChooser.setAcceptAllFileFilterUsed(false);
 
@@ -35,6 +37,18 @@ public class ExportPdfButton extends JPanel {
             // add .pdf if not defined
             if (!filePath.toLowerCase().endsWith(".pdf")) {
                 filePath += ".pdf";
+            }
+
+            File file = new File(filePath);
+            if (file.exists()) {
+                int response = JOptionPane.showConfirmDialog(null,
+                        "Wil je de bestaande PDF overschrijven?",
+                        "Overschrijf",
+                        JOptionPane.OK_CANCEL_OPTION,
+                        JOptionPane.QUESTION_MESSAGE);
+                if (response == JOptionPane.CANCEL_OPTION) {
+                    return;
+                }
             }
             pdfRenderer.download(filePath);
         }
