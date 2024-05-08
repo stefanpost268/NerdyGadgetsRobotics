df36ef87c30ef89e1a57cd0d273374ceffa07369
Roa-el <s1201349@student.windesheim.nl>
2024-05-08 16:25:05 +0200

    NER-94 DEL: commented line

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,6 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-//    implementation("com.fazecast:jSerialComm:2.0.0,3.0.0")
     implementation("com.mysql:mysql-connector-j:8.3.0")
     implementation("org.xhtmlrenderer:flying-saucer-pdf:9.7.2")
     implementation("com.mysql:mysql-connector-j:8.3.0")
