1940b916d6ea562b68d3b55b1ddcea511bbcc9da
Elnathan Pauwels <enpauwels@gmail.com>
2024-05-08 09:44:41 +0200

    NER-89 ADD: Orderpage

---
+++ src/main/java/pages/OrderPage.java
@@ -0,0 +1,15 @@
+package pages;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+
+
+public class OrderPage extends JPanel implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Implement this method
+    }
+    
+}
