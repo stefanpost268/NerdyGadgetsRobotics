5a4439e64296b40e0d99162b7034f1b17913ec3b
kolsm <kol.smits@hotmail.com>
2024-05-09 14:05:21 +0200

    NER-92 ADD: get stockitemID from productpage

--- src/main/java/pages/ProductPage.java
+++ src/main/java/pages/ProductPage.java
@@ -1,5 +1,8 @@
 package pages;
 
+import dialogs.ChangeStock;
+import models.StockItem;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
@@ -7,13 +10,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 
-import models.StockItem;
-
 public class ProductPage extends JPanel implements ActionListener {
 
     private JTextField searchField = new JTextField(20);
     private JButton searchButton = new JButton("Zoeken");
     private JTable table;
+    private JButton action = new JButton("Bewerk");
     private StockItem stockItem = new StockItem();
 
     public ProductPage() {
@@ -22,15 +24,18 @@ public class ProductPage extends JPanel implements ActionListener {
         JPanel searchPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         searchPanel.add(this.searchField);
         searchPanel.add(this.searchButton);
+        searchPanel.add(Box.createHorizontalStrut(936)); //set margin so button is right side of screen
+        searchPanel.add(this.action);
         add(searchPanel, BorderLayout.PAGE_START);
+        this.action.addActionListener(this);
         this.searchButton.addActionListener(this);
         this.searchField.addActionListener(this);
-
         renderTable();
     }
 
     private void renderTable() {
         search();
+        this.table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
         JScrollPane scrollPane = new JScrollPane(this.table);
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
@@ -43,7 +48,7 @@ public class ProductPage extends JPanel implements ActionListener {
 
     private void search() {
         List<StockItem> stockItems;
-        if(this.searchField.getText().isEmpty()) {
+        if (this.searchField.getText().isEmpty()) {
             stockItems = this.stockItem.get();
         } else {
             stockItems = this.stockItem.like("StockItemName", this.searchField.getText()).get();
@@ -54,7 +59,7 @@ public class ProductPage extends JPanel implements ActionListener {
                 this.stockItem.fillable()
         );
 
-        if(this.table == null) {
+        if (this.table == null) {
             this.table = new JTable(model);
         } else {
             this.table.setModel(model);
@@ -65,8 +70,17 @@ public class ProductPage extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource().equals(this.searchButton) || e.getSource().equals(this.searchField)) {
+        if (e.getSource().equals(this.searchButton) || e.getSource().equals(this.searchField)) {
             search();
+        }
+        if (e.getSource().equals(this.action)) {
+            int selectedRow = table.convertRowIndexToModel(table.getSelectedRow());
+            Object id = table.getValueAt(selectedRow, 0);
+            int intValue = Integer.parseInt(String.valueOf(id));
+
+            ChangeStock dialog = new ChangeStock(new JFrame(), intValue);
+
+
         }
     }
 }
