d1200528b3f13271cfdede61d3f53fcbbb59c2bd
stefanpost268 <97079064@st.deltion.nl>
2024-05-13 23:02:05 +0200

    NER-167 ADD: findUnfinishedOrders on OrderRepository

--- src/main/java/repositories/OrderRepository.java
+++ src/main/java/repositories/OrderRepository.java
@@ -1,9 +1,13 @@
 package repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import models.Order;
 
 public interface OrderRepository extends CrudRepository<Order, Integer>
 {
-
+    @Query("SELECT o FROM Order o WHERE o.Status != 'Done'")
+    Page<Order> findUnfinishedOrders(Pageable pageable);
 }
