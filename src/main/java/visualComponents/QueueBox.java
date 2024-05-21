84a3f6ee2637baca0aadf58223359208fd6fad11
HiddeXA <hiddealberts@gmail.com>
2024-05-21 12:52:56 +0200

    NER-138 ADD: added button on the bottom of the que table

--- src/main/java/visualComponents/QueueBox.java
+++ src/main/java/visualComponents/QueueBox.java
@@ -3,37 +3,52 @@ package visualComponents;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
 
-public class QueueBox extends JPanel {
+public class QueueBox extends JPanel implements ActionListener {
     private DefaultTableModel queueTableModel;
 
     public QueueBox(List<Object[]> queueData) {
         setBackground(Color.LIGHT_GRAY);
         setLayout(new FlowLayout(FlowLayout.LEFT)); // align left
         JLabel queueLabel = new JLabel("Bestelling wachtrij");
+        JButton executeButton = new JButton("Verwerken");
+        executeButton.addActionListener(this);
         add(queueLabel);
 
         //table model
         queueTableModel = new DefaultTableModel(
-                new Object[]{"Bestel Nr", "Product aantal", "Status", "Actie"}, 0
+                new Object[]{"Bestel Nr", "Product aantal", "Status"}, 0
         );
 
         //Create table
         JTable queueTable = new JTable(queueTableModel);
+        queueTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         queueTable.setDefaultEditor(Object.class, null); // read-only
         JScrollPane queueScrollPane = new JScrollPane(queueTable); // Add scroll to table
-        queueScrollPane.setPreferredSize(new Dimension(320, 310));
+        queueScrollPane.setPreferredSize(new Dimension(320, 270));
 
         queueTable.getTableHeader().setReorderingAllowed(false); // Stop user column swipe
         add(queueScrollPane);
 
+
+
+
         fillQueueTable(queueData);
+        //adding button to the bottom of the table
+        add(executeButton);
     }
 
     private void fillQueueTable(List<Object[]> data) {
         for (Object[] row : data) {
             queueTableModel.addRow(row);
         }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Button clicked");
     }
 }
