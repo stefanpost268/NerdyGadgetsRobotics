99b4f913f8b0c7317caba412122a8d9b266abb4b
Roa-el <s1201349@student.windesheim.nl>
2024-05-22 11:29:39 +0200

    NER-83 ADD: stockitemholdings model

--- src/main/java/models/StockItemHolding.java
+++ src/main/java/models/StockItemHolding.java
@@ -1,2 +1,18 @@
-package models;public class StockItemHolding {
+package models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "stockitemholdings")
+public class StockItemHolding {
+
+    @Id
+    private Integer StockItemID;
+
+    @Column()
+    private Integer QuantityOnHand;
+
+    public Integer getQuantityOnHand() {
+        return QuantityOnHand;
+    }
 }
