c4df2051b0df17aea7a1fba508c0d10b3af12357
RuudLeppers <150801047+RuudLeppers@users.noreply.github.com>
2024-05-23 14:16:45 +0200

    NER-93 IMP: leesbaarheid code

--- src/main/java/services/PdfRenderer.java
+++ src/main/java/services/PdfRenderer.java
@@ -2,10 +2,10 @@ package services;
 
 import helpers.DatabaseConnector;
 import org.xhtmlrenderer.pdf.ITextRenderer;
+
 import java.io.File;
 import java.io.OutputStream;
 import java.nio.file.Files;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.regex.Matcher;
@@ -13,21 +13,20 @@ import java.util.regex.Pattern;
 
 public class PdfRenderer extends ITextRenderer {
 
-    public PdfRenderer() {
+    private final int orderID;
+
+    public PdfRenderer(int orderID) {
         super();
+        this.orderID = orderID;
     }
 
     public void download(String outputFile) {
         try {
-            // Modify the HTML content in-memory
             String html = modifyHtmlContent(Files.readString(getHtmlFile().toPath()));
-
-            PdfRenderer renderer = new PdfRenderer();
-
-            renderer.setDocumentFromString(html);
-            renderer.layout();
+            this.setDocumentFromString(html);
+            this.layout();
             try (OutputStream os = new java.io.FileOutputStream(outputFile)) {
-                renderer.createPDF(os);
+                this.createPDF(os);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -40,53 +39,61 @@ public class PdfRenderer extends ITextRenderer {
 
     private String modifyHtmlContent(String htmlContent) {
         DatabaseConnector database = new DatabaseConnector();
+        String[] customerData = database.getCustomerData(orderID);
+        List<Object[]> orderLines = database.getOrderlines(orderID);
 
         Pattern pattern = Pattern.compile("\\{\\{([^{}]*)\\}\\}");
         Matcher matcher = pattern.matcher(htmlContent);
-
         StringBuilder modifiedHtmlContent = new StringBuilder();
-
-
         int lastIndex = 0;
+
         while (matcher.find()) {
-            String value = matcher.group(1);
+            String placeholder = matcher.group(1);
             modifiedHtmlContent.append(htmlContent, lastIndex, matcher.start());
 
-            int OrderID = 4;
-            String[] customerData = database.getCustomerData(OrderID);
-            List<Object[]> orderlines = database.getOrderlines(OrderID);
-
-            String replacementID = Objects.equals(value, "Bestellings nummer:") ? "Bestellings nummer: " + customerData[0] : "" ; // vervang customerid
-            String replacementNaam = Objects.equals(value, "Naam:") ? "Naam: " + customerData[1]: "" ; // vervang customernaam
-            String replacementPhonenumber = Objects.equals(value, "Telefoonnummer:") ? "Telefoonnummer: " + customerData[2]: "" ; // vervang telefoonnummer
-            String replacementadres = Objects.equals(value, "Adres:") ? "Adres: " + customerData[3]: "" ; // vervang adres
-            String replacementpostcode = Objects.equals(value, "postcode:") ? "postcode: " + customerData[4]: "" ; // vervang postcode
-            String replacementOrderdatum = Objects.equals(value, "Bestel datum:") ? "Bestel datum: " + customerData[5]: "" ; // vervang datum
-
-            if (Objects.equals(value, "orderlines")) {
-                for (Object[] row : orderlines) {
-                    int stockItemID = (int) row[0];
-                    int quantity = (int) row[1];
-                    String description = (String) row[2];
-                    modifiedHtmlContent.append("<tr>")
-                            .append("<td>").append(stockItemID).append("</td>")
-                            .append("<td>").append(description).append("</td>")
-                            .append("<td>").append(quantity).append("</td>")
-                            .append("</tr>");
-                }
-            }
-            //String replacementStockitemid = Objects.equals(value, "ITEMID") ? Arrays.toString(customerDataList) : "" ; // vervang itemid
-            //String replacementquantity = Objects.equals(value, "PRODUCT") ? Arrays.toString(customerDataList) : "" ; // vervang product
-            // String replacementdescription = Objects.equals(value, "AANTAL") ? Arrays.toString(customerDataList) : "" ; // vervang aantal
-
-
-            modifiedHtmlContent.append(replacementID).append(replacementadres).append(replacementNaam).append(replacementPhonenumber).append(replacementpostcode).append(replacementOrderdatum) ;//.append(replacementStockitemid). append(replacementquantity).append(replacementdescription);
+            String replacement = getReplacementForPlaceholder(placeholder, customerData, orderLines);
+            modifiedHtmlContent.append(replacement);
 
             lastIndex = matcher.end();
         }
 
         modifiedHtmlContent.append(htmlContent.substring(lastIndex));
-
         return modifiedHtmlContent.toString();
+    }
+
+    private String getReplacementForPlaceholder(String placeholder, String[] customerData, List<Object[]> orderLines) {
+        switch (placeholder) {
+            case "Bestellings nummer:":
+                return "Bestellings nummer: " + customerData[0];
+            case "Naam:":
+                return "Naam: " + customerData[1];
+            case "Telefoonnummer:":
+                return "Telefoonnummer: " + customerData[2];
+            case "Adres:":
+                return "Adres: " + customerData[3];
+            case "postcode:":
+                return "postcode: " + customerData[4];
+            case "Bestel datum:":
+                return "Bestel datum: " + customerData[5];
+            case "orderlines":
+                return generateOrderLinesTable(orderLines);
+            default:
+                return "{{" + placeholder + "}}";
+        }
+    }
+
+    private String generateOrderLinesTable(List<Object[]> orderLines) {
+        StringBuilder tableHtml = new StringBuilder();
+        for (Object[] row : orderLines) {
+            int stockItemID = (int) row[0];
+            int quantity = (int) row[1];
+            String description = (String) row[2];
+            tableHtml.append("<tr>")
+                    .append("<td>").append(stockItemID).append("</td>")
+                    .append("<td>").append(description).append("</td>")
+                    .append("<td>").append(quantity).append("</td>")
+                    .append("</tr>");
+        }
+        return tableHtml.toString();
     }
 }
