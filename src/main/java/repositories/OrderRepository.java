cb48c9d37e1fa296dc66533fb86164c7751a6a07
stefanpost268 <97079064@st.deltion.nl>
2024-05-27 12:25:44 +0200

    NER-198 FIX: order on status for findUnfinishedOrders in OrderRepository

--- src/main/java/repositories/OrderRepository.java
+++ src/main/java/repositories/OrderRepository.java
@@ -8,7 +8,7 @@ import models.Order;
 
 public interface OrderRepository extends CrudRepository<Order, Integer>
 {
-    @Query("SELECT o FROM Order o WHERE o.Status != 'Done'")
+    @Query("SELECT o FROM Order o WHERE o.Status != 'Done' ORDER BY CASE WHEN o.Status = 'InProgress' THEN 0 WHEN o.Status = 'Open' THEN 2 ELSE 1 END, o.OrderDate")
     Page<Order> findUnfinishedOrders(Pageable pageable);
 
     @Query("SELECT o FROM Order o WHERE o.Status != 'Done'")
